dd5f27f38683e2cad7149eaf20cb2c56f168cf44
Roger Cristian <erogennin@gmail.com>
2024-02-21 21:35:54 -0300

    commit de teste do repos importado do azure

--- Assets/Scripts/Ui/ClientScreen.cs
+++ Assets/Scripts/Ui/ClientScreen.cs
@@ -1,29 +1,16 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using UnityEngine.UI;
-
 public class ClientScreen : MonoBehaviour
 {
-   [SerializeField] private Button button;
+    [SerializeField] private Button button;
 
     private void Awake()
     {
-      
-       
         button.onClick.AddListener(() =>
-        {
-            //GameManager.Instance.OnChangeState += GameManager_OnChangeState;
-            GameManager.Instance.PlayerReadyScreen();
-            Hide();
-        });
+       {
+           //GameManager.Instance.OnChangeState += GameManager_OnChangeState;
+           GameManager.Instance.PlayerReadyScreen();
+           Hide();
+       });
     }
-
-    //private void GameManager_OnChangeState(object sender, System.EventArgs e)
-    //{
-        
-    //    GameManager.Instance.Teste();
-    //}
 
     private void Hide()
     {
